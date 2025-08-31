9f458dbd6f17609d2fede92d3f5adc01810fc892
Rahul De <rahul080327@gmail.com>
2025-08-31 10:28:32 +0100

    [stream] better stream

--- main.go
+++ main.go
@@ -9,14 +9,18 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"errors"
 	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 
-	"github.com/nxadm/tail"
+	"github.com/fsnotify/fsnotify"
 )
 
 const DIR_NAME = "logs"
@@ -24,6 +28,43 @@ const DIR_NAME = "logs"
 func errOut(w http.ResponseWriter, err error) {
 	w.WriteHeader(http.StatusInternalServerError)
 	fmt.Fprint(w, err.Error())
+}
+
+func streamDiff(
+	file *os.File,
+	currentPos *int64,
+	w http.ResponseWriter,
+	flusher http.Flusher,
+	ctx context.Context,
+) {
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return
+	}
+
+	if fileInfo.Size() < *currentPos {
+		// file truncated
+		*currentPos = 0
+		file.Seek(0, io.SeekStart)
+	}
+
+	if fileInfo.Size() > *currentPos {
+		// file has new content
+		file.Seek(*currentPos, io.SeekStart)
+		scanner := bufio.NewScanner(file)
+
+		for scanner.Scan() {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				fmt.Fprintln(w, scanner.Text())
+				flusher.Flush()
+			}
+		}
+
+		*currentPos = fileInfo.Size()
+	}
 }
 
 func ping(w http.ResponseWriter, r *http.Request) {
@@ -76,25 +117,79 @@ func get(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	t, err := tail.TailFile(log, tail.Config{Follow: true, ReOpen: true})
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Transfer-Encoding", "chunked")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+
+	ctx, cancel := context.WithCancel(r.Context())
+	defer cancel()
+
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		errOut(w, errors.New("Streaming unsupported"))
+		return
+	}
+
+	var startPos int64 = 0
+	file, err := os.Open(log)
+	if err != nil {
+		errOut(w, err)
+		return
+	}
+	defer file.Close()
+
+	_, err = file.Seek(startPos, io.SeekStart)
 	if err != nil {
 		errOut(w, err)
 		return
 	}
 
-	flusher, ok := w.(http.Flusher)
-	if !ok {
-		errOut(w, errors.New("Cannot make a HTTP flusher"))
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			fmt.Fprintln(w, scanner.Text())
+			flusher.Flush()
+		}
+	}
+
+	currentPos := startPos
+	ticker := time.NewTicker(100 * time.Millisecond)
+	defer ticker.Stop()
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		errOut(w, err)
+	}
+	defer watcher.Close()
+
+	if err = watcher.Add(log); err != nil {
+		errOut(w, err)
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Connection", "Keep-Alive")
-	w.Header().Set("X-Content-Type-Options", "nosniff")
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case event := <-watcher.Events:
+			if event.Name == log {
+				if event.Op&fsnotify.Remove == fsnotify.Remove {
+					return
+				}
 
-	for line := range t.Lines {
-		fmt.Fprintln(w, line.Text)
-		flusher.Flush()
+				if event.Op&fsnotify.Write == fsnotify.Write || event.Op&fsnotify.Create == fsnotify.Create {
+					streamDiff(file, &currentPos, w, flusher, ctx)
+				}
+			}
+		case <-ticker.C:
+			// Periodic check in case we missed a file event, needed?
+			streamDiff(file, &currentPos, w, flusher, ctx)
+		case err := <-watcher.Errors:
+			slog.Error("Watcher error", "err", err)
+		}
 	}
 }
 
